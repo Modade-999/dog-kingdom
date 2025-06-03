40167c2ec6bd49b4aa18bb43b35de2cb0fc4f80b
Modade-999 <adelowomodadeoluwa@gmail.com>
2025-06-03 20:44:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let cart = [];
 
+document.querySelectorAll(".adopt-btn").forEach(button => {
+    button.addEventListener("click", () => {
+        const dogName = button.dataset.name;
+        cart.push(dogName);
+        alert(`${dogName} added to your adoption list!`);
+    });
+});
